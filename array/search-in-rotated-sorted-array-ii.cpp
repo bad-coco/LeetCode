62b96a260d88def13e055bf9488a93c220bda39b
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-05-01 17:43:48 +0530

    LC-search-in-rotated-sorted-array-ii [Runtime: N/A; Memory: N/A]

---
+++ array/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool search(vector<int>& arr, int target) {
+        int n= arr.size();
+        int low =0,high =n-1;
+        
+        while(low<=high)
+        {
+            int mid =low +(high-low)/2;
+            if(arr[mid]==target) return true;
+            //left sorted
+            if(arr[low]<arr[mid])
+            {
+                //low<=target<mid
+                if(arr[low]<=target and target <arr[mid])
+                {
+                    high = mid-1;
+                }
+                else{
+                    low= mid+1;
+                }
+
+            }
+            //right sorted
+            else {
+                // mid<target<=high
+
+                if(arr[mid]<target and target<=arr[high])
+                {
+                    low=mid+1;
+                }
+                else {
+                    high=mid-1;
+                }
+
+            }
+        }
+        return false;
+    }
+};
