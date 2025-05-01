0ab2b9c961bab65065895a545d895e307490724c
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-05-01 14:10:46 +0530

    LC-search-in-rotated-sorted-array [Runtime: 0 ms; Memory: 15.2 MB]

---
+++ binary-search/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int index = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } 
+            
+           if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target and arr[mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else if (arr[mid] < arr[high]) {
+                if (arr[high] >= target and arr[mid] < target) {
+                    low = mid + 1;
+
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return index;
+    }
+};
