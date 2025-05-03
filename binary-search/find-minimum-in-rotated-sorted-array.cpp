1333742736cbcd48d3c31a0c38002807b4b5593f
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-05-03 12:40:31 +0530

    LC-find-minimum-in-rotated-sorted-array [Runtime: 0 ms; Memory: 14 MB]

---
+++ binary-search/find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& arr) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int minNum = INT_MAX;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] < minNum) {
+                minNum = arr[mid];
+            }
+            if (arr[low] <= arr[mid]) {
+                minNum = arr[low] < minNum ? arr[low] : minNum;
+                low = mid + 1;
+            } else {
+                minNum = arr[mid] < minNum ? arr[mid] : minNum;
+                high = mid - 1;
+            }
+           
+        }
+         return minNum;
+    }
+    };
