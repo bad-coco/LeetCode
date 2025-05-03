808e91fa36440a77c20b3968788b945dc550be59
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-05-03 12:44:45 +0530

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/find-minimum-in-rotated-sorted-array.cpp
+++ binary-search/find-minimum-in-rotated-sorted-array.cpp
@@ -4,20 +4,30 @@ public:
         int n = arr.size();
         int low = 0, high = n - 1;
         int minNum = INT_MAX;
-        while (low <= high) {
+        // while (low <= high) {
+        //     int mid = low + (high - low) / 2;
+        //     if (arr[mid] < minNum) {
+        //         minNum = arr[mid];
+        //     }
+        //     if (arr[low] <= arr[mid]) {
+        //         minNum = arr[low] < minNum ? arr[low] : minNum;
+        //         low = mid + 1;
+        //     } else {
+        //         minNum = arr[mid] < minNum ? arr[mid] : minNum;
+        //         high = mid - 1;
+        //     }
+
+        // }
+        //  return minNum;
+        while(low <= high) {
             int mid = low + (high - low) / 2;
-            if (arr[mid] < minNum) {
-                minNum = arr[mid];
-            }
-            if (arr[low] <= arr[mid]) {
-                minNum = arr[low] < minNum ? arr[low] : minNum;
+
+            if (arr[mid] > arr[high]) {
                 low = mid + 1;
             } else {
-                minNum = arr[mid] < minNum ? arr[mid] : minNum;
-                high = mid - 1;
+                high = mid-1;
             }
-           
         }
-         return minNum;
+        return arr[low];
     }
-    };
+};
