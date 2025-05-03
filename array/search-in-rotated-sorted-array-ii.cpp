ea82d8094b17f5a9e0230a54d926d5c7eeee7cfc
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-05-03 12:15:54 +0530

    LC-search-in-rotated-sorted-array-ii [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array-ii.cpp
+++ array/search-in-rotated-sorted-array-ii.cpp
@@ -1,50 +1,52 @@
 class Solution {
 public:
-    bool search(vector<int>& arr, int target) {
-        int n= arr.size();
-        int low =0,high =n-1;
-        
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low =0, high= n-1;
+        int ans = false;
         while(low<=high)
         {
-            int mid =low +(high-low)/2;
-            if(arr[mid]==target) return true;
-
-            // duplicate value
-            if(arr[low] == arr[mid] and arr[high]==arr[mid])
+            int mid = low + (high - low)/2;
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            if(nums[mid] == nums[low] and nums[mid] == nums[high])
             {
                 low++;
                 high--;
                 continue;
             }
-
-
-            //left sorted
-            if(arr[low]<=arr[mid])
+            // left sorted
+            if(nums[low]<=nums[mid])
             {
-                //low<=target<mid
-                if(arr[low]<=target and target <arr[mid])
+                // low<target<mid
+                if(nums[low]<target and target <= nums[mid])
                 {
                     high = mid-1;
                 }
                 else{
-                    low= mid+1;
+                    low = mid+1;
                 }
+
 
             }
-            //right sorted
-            else {
-                // mid<target<=high
+            //right sorted check 
+            else{
+                // mid<target<high
 
-                if(arr[mid]<target and target<=arr[high])
+                if(nums[mid]<target and target <=nums[high])
                 {
-                    low=mid+1;
+                    low = mid+1;
                 }
-                else {
-                    high=mid-1;
+                else{
+                    high = mid -1 ;
                 }
+
 
             }
         }
         return false;
+        
     }
 };
