4dca5c78fc590da438e61ae68b644468714ca755
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-05-07 15:43:52 +0530

    LC-find-the-index-of-the-first-occurrence-in-a-string [Runtime: N/A; Memory: N/A]

--- string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,10 +1,34 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        size_t found = haystack.find(needle);
-        if (found != string::npos) {
-            return found;
+        int l1= haystack.size(), l2= needle.size();
+  
+        int result=-1;
+        
+        if(l2>l2) return result;
+        int index1=0,index2=0;
+
+        while(index1<l1)
+        {
+            if(haystack[index1]==needle[index2])
+            {
+               index1++;
+                index2++;
+            }
+            else {
+                 index1++;
+                index2=0;
+            }
+             
+              if(index2==l2)
+              {
+                return index1-l2;
+              }
+
         }
-        return -1;
+        return result;
     }
 };
+
+// haystack = "sadbutsad", needle = "sad"
+// haystack = "sbutsad", needle = "sad"
