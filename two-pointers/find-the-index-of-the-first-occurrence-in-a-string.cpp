a3e32e633680c6fe833db979498b3152bb95f4f1
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-05-07 16:02:20 +0530

    LC-find-the-index-of-the-first-occurrence-in-a-string [Runtime: 0 ms; Memory: 8.6 MB]

--- two-pointers/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ two-pointers/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
+    // haystack = "mississippi", needle = "issip"
     int strStr(string haystack, string needle) {
-        int l1= haystack.size(), l2= needle.size();
-  
-        int result=-1;
-        
-        if(l2>l2) return result;
-        int index1=0,index2=0;
+        int l1 = haystack.size(), l2 = needle.size();
 
-        while(index1<l1)
-        {
-            if(haystack[index1]==needle[index2])
-            {
-               index1++;
+        int startIndex = -1;
+
+        if (l2 > l1)
+            return startIndex;
+        int index1 = 0, index2 = 0;
+
+        while (index1 < l1) {
+            if (haystack[index1] == needle[index2]) {
+
                 index2++;
-            }
-            else {
-                 index1++;
-                index2=0;
-            }
-             
-              if(index2==l2)
-              {
-                return index1-l2;
-              }
+            } else {
 
+                index1 = index1 - index2;
+                index2 = 0;
+            }
+            if (index2 >= l2) {
+                return index1 - index2 + 1;
+            }
+            index1++;
         }
-        return result;
+        return startIndex;
     }
 };
 
