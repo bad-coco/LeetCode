32952354ad01f283695e84af308258aac78df16e
bad-coco <146406262+bad-coco@users.noreply.github.com>
2025-05-13 11:17:08 +0530

    LC-minimum-number-of-days-to-make-m-bouquets [Runtime: N/A; Memory: N/A]

---
+++ binary-search/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int maxElement(vector<int>& bloomDay, int n) {
+        int maxE = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            maxE=max(maxE, bloomDay[i]);
+        }
+        return maxE;
+    }
+
+    int findBouquets(vector<int>& bloomDay, int n, int currentDay, int m,
+                     int k) {
+        int count = 0;
+        int bq = 0;
+        for (int i = 0; i < n; i++) {
+            if (bloomDay[i] <= currentDay) {
+                count++;
+            } else {
+                bq += count / k;
+                count = 0;
+                if (bq > m) {
+                    return bq;
+                }
+            }
+        }
+        bq += count / k;
+        return bq;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
+        int ans = -1;
+        if (m * k > n) {
+            return -1;
+        }
+         
+        int maxBloomDay = maxElement(bloomDay, n);
+
+        int low = 1, high = maxBloomDay;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int bouquetes = findBouquets(bloomDay, n, mid, m, k);
+            if (bouquetes >= m) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
