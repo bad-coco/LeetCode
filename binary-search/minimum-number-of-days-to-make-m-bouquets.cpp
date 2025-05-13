23a6f35ad87b15da5bd7caa50753640e24d63337
bad-coco <146406262+bad-coco@users.noreply.github.com>
2025-05-13 11:22:12 +0530

    LC-minimum-number-of-days-to-make-m-bouquets [Runtime: 18 ms; Memory: 70.4 MB]

--- binary-search/minimum-number-of-days-to-make-m-bouquets.cpp
+++ binary-search/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -30,7 +30,7 @@ public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
         int ans = -1;
-        if ((long)(m * k) > n) {
+        if ((long)m * k > n) {
             return -1;
         }
          
